f23ea3f4383c96c59807ccde5edb200f907dbe56
Tamara Lima <tlr@DESKTOP-HT3HH80.tlr.com>
2019-05-29 07:55:23 -0300

    incluido no main grafo k5 e k3,3 para checar planaridade

--- GrafoApi.py
+++ GrafoApi.py
@@ -33,20 +33,62 @@ class GrafoApi():
 # 		        quit()
 # 		    else:
 # 		        print "opção inexistente"
-		grafo = Grafo('Grafo01')
-		grafo.criarVertice('a')
-		grafo.criarVertice('b')
-		grafo.criarVertice('c')
-		grafo.criarVertice('d')
-		grafo.inserirAresta('---1---',10,'a','b')
-		grafo.inserirAresta('---2---',20,'a','c')
-		grafo.inserirAresta('---3---',30,'b','c')
-		grafo.inserirAresta('---4---',40,'c','d')
-		grafo.exibirGrafo()
-		# if grafo.verificarAdjacente('b','d') ==True:
-		# 	print('é adjascente')
-		# else: 
-		# 	print('não é adjascente')
+		grafo = Grafo('Grafok5')
+		grafo.criarVertice('V1')
+		grafo.criarVertice('V2')
+		grafo.criarVertice('V3')
+		grafo.criarVertice('V4')
+		grafo.criarVertice('V5')
+
+		grafo.inserirAresta('---A1---',20,'V1','V2')
+		grafo.inserirAresta('---A2---',40,'V1','V5')
+		grafo.inserirAresta('---A3---',10,'V1','V3')
+		grafo.inserirAresta('---A4---',30,'V1','V4')
+
+		grafo.inserirAresta('---A5---',40,'V2','V5')
+
+		grafo.inserirAresta('---A6---',40,'V2','V3')
+		grafo.inserirAresta('---A7---',40,'V5','V3')
+		grafo.inserirAresta('---A8---',40,'V4','V3')
+
+		grafo.inserirAresta('---A9---',40,'V4','V2')
+		grafo.inserirAresta('---A10---',40,'V4','V5')
+
+		r = ""
+		if grafo.grafoConexoEhPlanar():
+			r= "O Grafo é Planar!\n" 
+		else: 
+			r="O Grafo não é Planar!\n"
+
+		print(r)
+		grafo2 = Grafo("Grafo k(3,3)")
+		grafo2.criarVertice('V1')
+		grafo2.criarVertice('V2')
+		grafo2.criarVertice('V3')
+		grafo2.criarVertice('V4')
+		grafo2.criarVertice('V5')
+		grafo2.criarVertice('V6')
+
+		grafo2.inserirAresta('---A1---',10,'V1','V4')
+		grafo2.inserirAresta('---A2---',30,'V1','V5')
+		grafo2.inserirAresta('---A3---',20,'V1','V6')
+
+		grafo2.inserirAresta('---A4---',40,'V2','V4')
+		grafo2.inserirAresta('---A5---',40,'V2','V5')
+		grafo2.inserirAresta('---A6---',40,'V2','V6')
+
+		grafo2.inserirAresta('---A7---',40,'V3','V4')
+		grafo2.inserirAresta('---A8---',40,'V3','V5')
+		grafo2.inserirAresta('---A9---',40,'V4','V6')
+
+		grafo2.exibirGrafo()
+
+		y = ""
+		if grafo2.grafoConexoEhPlanar():
+			y= "O Grafo é Planar!\n" 
+		else: 
+			y="O Grafo não é Planar!\n"
+
 		# a=grafo.pegarCustoAresta('---1---')
 		# print(a)
 		# b=grafo.retornarExtremosAresta('---4---')
@@ -55,8 +97,8 @@ class GrafoApi():
 		# # grafo.removerVertice('c')
 		# grafo.removerAresta('---1---')
 		# grafo.exibirGrafo()
-		c=grafo.BuscaEmLargura('a', 'd')
-		print('BuscaEmLargura V1: ',c.getNome())
+		#c=grafo.BuscaEmLargura('a', 'd')
+		#print('BuscaEmLargura V1: ',c.getNome())
 		# d=grafo.BuscaEmProfundidade('a', 'd')
 		# print('BuscaEmProfundidade V2: ',d.getNome())
 
